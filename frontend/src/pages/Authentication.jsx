b731630983ff93d43bf08eedde1de8dfbe97e5fa
xGabrisado <madeira.gabs@gmail.com>
2023-08-18 13:49:51 -0300

    🎨 Ajustando TCC

--- frontend/src/pages/Authentication.jsx
+++
@@ -1,9 +0,0 @@
-import SignIn from "../components/SignIn";
-
-export default function AuthenticationPage() {
-  return <SignIn />;
-}
-
-// export async function action({request}) {
-//   const data = await request.formData()
-// }
